7ab196e84c500f412a91e00f5c82418998857423
dov11 <ovo.dan@gmail.com>
2017-11-20 11:44:27 +0100

    add navbar and home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+User.destroy_all
+
 admin = User.create(email: "admin@gmail.com", password: "123456", admin: true)
-user1 = User.create(email: "example@gmail.com", password: "123456")
-user2 = User.create(email: "vak@gmail.com", password: "123456")
+student1 = User.create(email: "example@gmail.com", password: "123456")
+student2 = User.create(email: "vak@gmail.com", password: "123456")
