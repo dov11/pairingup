5fc7060c23065e9f5bb2eab74c007ddb7d5ed212
dov11 <ovo.dan@gmail.com>
2017-11-20 15:43:13 +0100

    random pairs generator

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,22 @@
 class Match < ApplicationRecord
+
+  def self.create_match
+    all_students = User.all.reject{|user| user.admin}
+
+    all_students_names = all_students.map{|student| student.profile.full_name}
+
+    hash = {}
+
+    array = all_students_names
+
+      while array.length>0
+        random_key = array[rand(array.length-1)]
+        hash[array[-1]] = random_key
+        array.reject!{|el| el == random_key}
+        array.pop
+      end
+
+      Match.create(pairing: hash)
+  end
+
 end
