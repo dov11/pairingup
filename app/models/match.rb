d09ae0a5b1e88d7acde09ed076b4df88cc20df1a
dov11 <ovo.dan@gmail.com>
2017-11-21 14:55:15 +0100

    add overwriting old listing and the ones after it

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,12 +3,9 @@ class Match < ApplicationRecord
   def self.create_match(date)
 
     if same_day?(date)
-      # x = Match.find_by(pairing_date: date)
-      # byebug
-      Match.find_by(pairing_date: date).destroy
-      Match.create(pairing: unique_pairings, pairing_date: date)
+      overwrite_this_and_later_matchings(date)
     else
-      Match.create(pairing: unique_pairings, pairing_date: date)
+      Match.create(pairing: unique_pairings(date), pairing_date: date)
     end
   end
 
@@ -19,9 +16,31 @@ class Match < ApplicationRecord
     Match.find_by(pairing_date: date).pairing_date.year == date.year
   end
 
-  def self.unique_pairings
+  def self.this_and_later_matchings(date)
+    Match.all.select{|match| match.pairing_date>=date}
+  end
+
+  def self.earlier_matchings(date)
+    Match.all.select{|match| match.pairing_date<date}
+  end
+
+  def self.matchings_dates(matchings)
+    matchings.map{|match| match.pairing_date}.sort
+  end
+
+  def self.overwrite_this_and_later_matchings(date)
+    matchings_dates = matchings_dates(this_and_later_matchings(date))
+    this_and_later_matchings(date).each do |match|
+      match.destroy
+    end
+    matchings_dates.each do |match_date|
+      Match.create(pairing: unique_pairings(match_date), pairing_date: match_date)
+    end
+  end
+
+  def self.unique_pairings(date)
     new_pairings = create_pairings(students_names)
-    while has_duplicates?(recent_pairings, new_pairings)
+    while has_duplicates?(recent_pairings(date), new_pairings)
       new_pairings = create_pairings(students_names)
     end
     new_pairings
@@ -32,22 +51,22 @@ class Match < ApplicationRecord
     all_students.map{|student| student.profile.full_name}
   end
 
-  def self.recent_pairings
+  def self.recent_pairings(date)
     number_of_students = students_names.length
     number_of_pairings = number_of_students - 1
-    Match.all.last(number_of_pairings-1).map{|match| match.pairing}
+    earlier_matchings(date).last(number_of_pairings-1).map{|match| match.pairing}
   end
 
   def self.create_pairings(submit_array)
-  hash = {}
-  array = submit_array
-  while array.length>0
-    random_key = array[rand(array.length-1)]
-    hash[array[-1]] = random_key
-    array.reject!{|el| el == random_key}
-    array.pop
-  end
-  return hash
+    hash = {}
+    array = submit_array
+    while array.length>0
+      random_key = array[rand(array.length-1)]
+      hash[array[-1]] = random_key
+      array.reject!{|el| el == random_key}
+      array.pop
+    end
+    return hash
   end
 
   def self.has_duplicates?(array_of_hashes, hash)
