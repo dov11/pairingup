96b7e2e05f97bbaa5c369a1af15c6cd60c561792
dov11 <ovo.dan@gmail.com>
2017-11-22 22:30:36 +0100

    clean up code of the match model

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,17 +3,24 @@ class Match < ApplicationRecord
   def self.pairings
     @@pairings
   end
+
   def self.create_match(date)
     @@pairings ||= create_array_of_pairings(students_names)
     if same_day?(date)
       match = Match.find_by(pairing_date: date)
       overwrite_this_and_later_matchings(match)
     else
-      # Match.create(pairing: unique_pairings(date), pairing_date: date)
       add_pairings(students_names) if pairings_run_out?
       create_consequent_match(date)
     end
+  end
+# block of methods for adding new matches
+  def self.pairings_run_out?
+    @@pairings.length==number_of_robin_rounds*Match.count
+  end
 
+  def self.add_pairings(students)
+    @@pairings.concat(create_array_of_pairings(students))
   end
 
   def self.create_consequent_match(date)
@@ -23,6 +30,16 @@ class Match < ApplicationRecord
     match.save
   end
 
+  def self.select_pairing(date)
+    match = Match.find_by(pairing_date: date)
+    @@pairings[match_index(match)]
+  end
+
+  def self.match_index(match)
+    Match.sort_by_created_asc.all.index(match)
+  end
+#---end of creating block
+# block of methods for overwriting previously created matchings
   def self.overwrite_this_and_later_matchings(match)
     shuffled_pairings=pairings_to_shuffle(match)
     pairings_to_shuffle_index=0
@@ -64,35 +81,10 @@ class Match < ApplicationRecord
     index_in_the_round_robin = match_index(match) % number_of_unique_matches
     number_of_pairings_to_shuffle = 5 - index_in_the_round_robin
   end
-
-  def self.pairings_run_out?
-    @@pairings.length==number_of_robin_rounds*Match.count
-  end
-
-  # def self.unique_pairings(date)
-  #   new_pairings = create_pairings(students_names)
-  #   while has_duplicates?(recent_pairings(date), new_pairings)
-  #     new_pairings = create_pairings(students_names)
-  #   end
-  #   new_pairings
-  # end
-  #creaing random pairings
-  # def self.create_pairings(submit_array)
-  #   hash = {}
-  #   array = submit_array
-  #   while array.length>0
-  #     random_key = array[rand(array.length-1)]
-  #     hash[array[-1]] = random_key
-  #     array.reject!{|el| el == random_key}
-  #     array.pop
-  #   end
-  #   return hash
-  # end
-
+# ---end of overwriting -block
+# block of round robin methods
   def self.create_array_of_pairings(students)
     array_of_pairings = []
-    # number_of_unique_matches = number_of_students-1
-
     fixed_element = students[0]
     shuffled_array = students.last(number_of_students-1)
 
@@ -103,20 +95,6 @@ class Match < ApplicationRecord
     end
     array_of_pairings.shuffle
   end
-
-  def self.add_pairings(students)
-    @@pairings.concat(create_array_of_pairings(students))
-  end
-
-  def self.match_index(match)
-    Match.sort_by_created_asc.all.index(match)
-  end
-
-  def self.select_pairing(date)
-    match = Match.find_by(pairing_date: date)
-    @@pairings[match_index(match)]
-  end
-
 
   def self.number_of_robin_rounds
     ((Match.all.length).to_f / (number_of_unique_matches)).ceil
@@ -139,62 +117,14 @@ class Match < ApplicationRecord
     end
     hash
   end
-
-
-  # def self.has_duplicates?(array_of_hashes, hash)
-  #   duplicate = false
-  #
-  #   array_of_hashes.each do |pairing|
-  #     hash.each do |student, its_match|
-  #       if pairing[student] == its_match
-  #         duplicate = true
-  #       end
-  #     end
-  #   end
-  #   return duplicate
-  # end
-
-  # def self.recent_pairings(date)
-  #   number_of_students = students_names.length
-  #   number_of_pairings = number_of_students - 1
-  #   # if Match.count != 0
-  #     # byebug
-  #     return earlier_matchings(date).last(number_of_pairings-1).map{|match| match.pairing}
-  #   # else
-  #     # return []
-  #   # end
-  # end
-  # --end create random pairings block
-
+# ---end of robin -block
+# block of helper methods
   def self.same_day?(date)
     return false unless Match.find_by(pairing_date: date)
     Match.find_by(pairing_date: date).pairing_date.day == date.day &&
     Match.find_by(pairing_date: date).pairing_date.month == date.month &&
     Match.find_by(pairing_date: date).pairing_date.year == date.year
   end
-
-  # def self.this_and_later_matchings(date)
-  #   Match.all.select{|match| match.pairing_date>=date}
-  # end
-  #
-  # def self.earlier_matchings(date)
-  #   Match.all.select{|match| match.pairing_date<date}
-  # end
-  #
-  # def self.matchings_dates(matchings)
-  #   matchings.map{|match| match.pairing_date}.sort
-  # end
-  #
-  # def self.overwrite_this_and_later_matchings(date)
-  #   matchings_dates = matchings_dates(this_and_later_matchings(date))
-  #   this_and_later_matchings(date).each do |match|
-  #     match.destroy
-  #   end
-  #   matchings_dates.each do |match_date|
-  #     Match.create(pairing: unique_pairings(match_date), pairing_date: match_date)
-  #   end
-  # end
-
 
   def self.students_names
     all_students = User.all.reject{|user| user.admin}
@@ -204,10 +134,7 @@ class Match < ApplicationRecord
   def self.number_of_students
     students_names.length
   end
-
-
-
-
+  #subblock of methods for displaying matches for students only
   def self.show_my_matches(user)
     Match.exclude_future_pairings.map do |match|
       match.select_my_pairings(user)
@@ -235,7 +162,7 @@ class Match < ApplicationRecord
   def users_partner(full_name)
     self.pairing[full_name] ? self.pairing[full_name] : self.pairing.keys[0]
   end
-
+  #----end of students -helper-block
   def self.sort_by_created_asc
     self.order('pairing_date asc')
   end
@@ -243,4 +170,5 @@ class Match < ApplicationRecord
   def nice_date
     "#{pairing_date.year}-#{pairing_date.month}-#{pairing_date.day}"
   end
+# ---end of helper-block
 end
