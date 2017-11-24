705a30303bfb95b78ba17e1cefc38f631cd5b909
dov11 <ovo.dan@gmail.com>
2017-11-24 12:33:01 +0100

    add profile tests

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid without a first_name" do
+      profile = Profile.new(first_name: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:first_name)
+    end
+
+    it "is invalid without a last name" do
+      profile = Profile.new(last_name: nil)
+      profile.valid?
+      expect(profile.errors).to have_key(:last_name)
+    end
+  end
+  describe ".full_name" do
+    let!(:user) {create :user}
+    let!(:profile) {create :profile, first_name: "First", last_name: "Last", user: user}
+    it "returns full name" do
+      expect(profile.full_name).to eq "First Last"
+    end
+  end
 end
