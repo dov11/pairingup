7eca8d0fb813cb98266abf68227599c0f1ba0cb2
dov11 <ovo.dan@gmail.com>
2017-11-24 15:36:33 +0100

    clean up code2

--- spec/features/list_of_users_spec.rb
+++ spec/features/list_of_users_spec.rb
@@ -3,11 +3,10 @@ describe "Current user viewing the list of users" do
   before { login_as admin }
   let!(:admin) {create :user, email: "admin_@gmail.com", admin: true}
   let!(:user1) {create :user, email: "user1@gmail.com", password: "123456", admin: false}
-  # let!(:user2) {create :user, email: "user2@gmail.com", admin: false}
 
   let!(:adminProfile) {create :profile, first_name: "Admin", last_name: "The Admin", user: admin}
   let!(:profile1) {create :profile, first_name: "1", last_name: "A", user: user1}
-  # let!(:profile2) {create :profile, first_name: "2", last_name: "B", user: user2}
+  
   def login(email, password)
     fill_in "Email", with: email
     fill_in "Password", with: password
